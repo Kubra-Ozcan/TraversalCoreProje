ed24921c2f36f794908cc62ad28fa1bbc26655f6
kubra <kubra@LAPTOP-CH16DCVB>
2023-09-15 14:44:43 +0300

    Add project files.

---
+++ DataAAccessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAAccessLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sa/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sa
+{
+    public class Class1
+    {
+    }
+}
